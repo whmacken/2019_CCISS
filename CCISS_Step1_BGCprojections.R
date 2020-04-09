3beee47b1be533c2fc2b4aa479a52445b9d9c923
Will MacKenzie <34073318+whmacken@users.noreply.github.com>
2020-04-09 04:35:14 -0700

    mostly Colin's changes

--- CCISS_Step1_BGCprojections.R
+++ CCISS_Step1_BGCprojections.R
@@ -16,15 +16,17 @@ source("./_CCISS_Parameters.R") ## settings used through all scripts
 grid <- "BC2kmGrid"
 
 ### Load random forest model
-model <- "18Var_6_2"
-fname <- "inputs/models/WNAv11_18_VAR_SubZone_ranger.Rdata"
+model <- "35Var_6_2"
+fname <- "inputs/models/WNAv11_35_VAR_SubZone_ranger.Rdata"
 load(fname)
 #rownames(importance(BGCmodel)) ### shows the variable used in the RFmodel
 vars <- as.data.frame(BGCmodel$variable.importance)
 vars <- row.names(vars)
 # setwd('C:/GitHub/2019_CCISS')
 
-
+Columns <- unique(c("PPT05", "PPT06", "PPT07", "PPT08", "PPT09", "PPT_at", 
+                    "PPT_wt", "CMD07", "CMD", "MAT", "PPT_sm", "Tmin_wt", "Tmax_sm",
+                    vars[!vars %in% c("PPT_MJ", "PPT_JAS", "PPT.dormant", "CMD.def", "CMDMax", "CMD.total")]))
 
 # ===============================================================================
 # BGC Projections for reference period
@@ -44,7 +46,7 @@ Y0 <- Y0 %>% dplyr::select(all_of(vars))
 ## Predict future subzones######
 BGC.pred.ref <- predict(BGCmodel, Y0)
 #dir.create("./outputs")
-fwrite(list(BGC.pred.ref$predictions), paste("outputs/BGC.pred.ref", grid, "csv", sep = "."), 
+fwrite(list(BGC.pred.ref$predictions), paste("outputs/BGC.pred", grid, "ref", model,".csv", sep = "."), 
           row.names = F)
 
 ## Write Climate file ######
@@ -73,7 +75,7 @@ for (hist.year in hist.years){
 BGC.pred <- predict(BGCmodel, Y0)
   
 #assign(paste("BGC.pred", hist.year, sep = "."), 
-  fwrite(list(BGC.pred$predictions), paste0("outputs/BGC.pred", grid, hist.year, ".csv", sep = "."), row.names = F)
+  fwrite(list(BGC.pred$predictions), paste("outputs/BGC.pred", grid, hist.year, model, "csv", sep = "."), row.names = F)
   
   ## Write Climate file ######
   # fwrite(Y0, paste("inputs/", grid, "_", hist.year, "_", model, ".csv",  sep = ""), row.names = F)
@@ -100,7 +102,7 @@ Y0 <- Y0 %>% dplyr::select(vars)
 
 ## Predict BGC units######
 BGC.pred.2018 <- predict(BGCmodel, Y0)
-fwrite(list(BGC.pred.2018$predictions), paste("outputs/BGC.pred", grid, "2018.csv", sep = "."), 
+fwrite(list(BGC.pred.2018$predictions), paste("outputs/BGC.pred", grid, "2018", model, "csv", sep = "."), 
           row.names = F)
 
 ## Write Climate file ######
@@ -140,7 +142,7 @@ for (hist.year in hist.years){
   
   ## Predict future subzones######
   BGC.pred <- predict(BGCmodel, get(paste("Y", hist.year, sep = ".")))
-  fwrite(list(BGC.pred$predictions), paste("outputs/BGC.pred", grid, hist.year, "csv", sep = "."), row.names = F)
+  fwrite(list(BGC.pred$predictions), paste("outputs/BGC.pred", grid, hist.year, model, "csv", sep = "."), row.names = F)
   
   ## Write Climate file ######
   # fwrite(get(paste("Y", hist.year, sep = ".")), paste("inputs/", grid, "_", hist.year, "_", model, ".csv", sep = ""), row.names = F)
@@ -151,18 +153,27 @@ for (hist.year in hist.years){
 # ===============================================================================
 # BGC Projections for future periods
 # ===============================================================================
+library(tidyr)
 
 GCM=GCMs[1]
 rcp=rcps[1]
 proj.year=proj.years[1]
+
 Columns <- c("Year",  Columns)
 ##===============
 ###Option if one big all GCM file
 Y0 <- fread("./inputs/BC2kmGrid_90 GCMsMSY.csv", select = Columns, stringsAsFactors = FALSE, data.table = FALSE)
+
+
+for(GCM in GCMs[11:15]){
+
+Y0 <- fread(paste0("./inputs/",grid,"_",GCM,".csv", sep = ""), select = c("Year", "ID1","ID2", Columns), data.table = F)   ##
+
 Y0 <- separate(Y0, Year, into = c("GCM","rcp","proj.year"), sep = "_", remove = T)
 Y0$proj.year <- gsub(".gcm","",Y0$proj.year)
 
 Y0 <- addVars(Y0)
+
 
 Y0 <- Y0 %>% dplyr::select(GCM, rcp,proj.year, vars)
 
@@ -179,6 +190,7 @@ Y0 <- Y0 %>% dplyr::select(GCM, rcp,proj.year, vars)
 # 
 # Y0 <- Y0 %>% select(GCM, rcp,proj.year, vars)
 ##======================
+
 require(doParallel)
 set.seed(123321)
 coreNum <- as.numeric(detectCores()-2)
@@ -186,16 +198,16 @@ cl <- makeCluster(coreNum)
 registerDoParallel(cl, cores = coreNum)
 
 out <- foreach(rcp = rcps, .combine = rbind) %:%
-  foreach(proj.year = proj.years, .combine = rbind, .packages = c("data.table","ranger", "dplyr")) %dopar% {
+  foreach(proj.year = proj.years, .combine = rbind, .packages = c("data.table","ranger", "dplyr")) %do% {  # %do% is faster and better than %dopar% (doesn't accumulate memory)
     sub <- Y0[which(Y0$GCM == GCM & Y0$rcp == rcp & Y0$proj.year == proj.year),]
     subPred <- predict(BGCmodel, sub)
     
-    fwrite(list(subPred$predictions), paste("outputs/BGC.pred", grid, GCM, rcp, proj.year, ".csv", sep = ""))
+    fwrite(list(subPred$predictions), paste("outputs/BGC.pred", grid, GCM, rcp, proj.year, model, "csv", sep = "."))
     # temp <- aggregate(ID ~ BGC.pred, data = subPred, FUN = length) %>% 
     #   mutate(GCM = GCM, rcp = rcp, proj.year = proj.year)
     # temp
     print(GCM)
   }
-
+}
 stopCluster(cl)
 
