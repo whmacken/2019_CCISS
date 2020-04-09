9b84619904f5fe6cd669c3d0afafa3f7c89f3759
cmahony <colin.mahony@gov.bc.ca>
2020-04-08 21:45:20 -0700

    removed the "columns <-" because it requires a variable (vars)

--- _CCISS_Parameters.R
+++ _CCISS_Parameters.R
@@ -5,10 +5,6 @@ GCMs <- c("ACCESS1-0", "CanESM2", "CCSM4", "CESM1-CAM5", "CNRM-CM5", "CSIRO-Mk3-
           "GFDL-CM3", "GISS-E2R", "HadGEM2-ES", "INM-CM4", "IPSL-CM5A-MR", "MIROC-ESM", 
           "MIROC5", "MPI-ESM-LR", "MRI-CGCM3")
 
-Columns <- unique(c("PPT05", "PPT06", "PPT07", "PPT08", "PPT09", "PPT_at", 
-                    "PPT_wt", "CMD07", "CMD", "MAT", "PPT_sm", "Tmin_wt", "Tmax_sm",
-                    vars[!vars %in% c("PPT_MJ", "PPT_JAS", "PPT.dormant", "CMD.def", "CMDMax", "CMD.total")]))
-
 edatopes <- c("B2", "C4", "D6")
 
 spps.lookup <- fread("inputs/Tree speciesand codes_2.0_2May2019.csv")
