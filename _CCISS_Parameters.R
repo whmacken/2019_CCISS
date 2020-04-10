b4dee33729208237066abbad0e049b265515881d
cmahony <colin.mahony@gov.bc.ca>
2020-04-09 21:02:03 -0700

    Step 2 and 2b reversion to Colin's code, with some updates

--- _CCISS_Parameters.R
+++ _CCISS_Parameters.R
@@ -1,6 +1,8 @@
 
 # Setting applied to all runs
 
+grid <- "BC2kmGrid"
+
 GCMs <- c("ACCESS1-0", "CanESM2", "CCSM4", "CESM1-CAM5", "CNRM-CM5", "CSIRO-Mk3-6-0", 
           "GFDL-CM3", "GISS-E2R", "HadGEM2-ES", "INM-CM4", "IPSL-CM5A-MR", "MIROC-ESM", 
           "MIROC5", "MPI-ESM-LR", "MRI-CGCM3")
@@ -9,7 +11,6 @@ edatopes <- c("B2", "C4", "D6")
 
 edatope.name <- c("Subxeric-poor", "Mesic-medium", "Hygric-rich")
 
-
 rcps <- c("rcp45", "rcp85")
 
 proj.years <- c(2025, 2055, 2085)
@@ -17,6 +18,9 @@ proj.years <- c(2025, 2055, 2085)
 hist.years <- c(1995, 2004, 2005, 2009, 2014, 2018)
 
 hist.year.name <- c("1991-2000", "1991-2018", "2001-2010", "2001-2018","2011-2018", "2018")
+
+model <- "35Var_6_2"
+fname <- "inputs/models/WNAv11_35_VAR_SubZone_ranger.Rdata"
 
 ####Lookup tables
 
